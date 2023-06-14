2c2d9fd1ffd1cb8d98b4370eaa90017cb7821c02
Samira Haddad <60987282+samyhad@users.noreply.github.com>
2023-06-14 20:56:31 -0300

    Update Cliente.java

--- Napster/src/RMIExample/Cliente.java
+++ Napster/src/RMIExample/Cliente.java
@@ -1,10 +1,16 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.Registry;
+import model.ServicoHora;
+import model.Hora;
 
 public class Cliente {
 
   public static void main(String[] args){
     Registry reg = LocateRegistry.getRegistry();
-    reg.lookup("rmi://127.0.0.1/servicohora")
+    ServicoHora shc = (ServicoHora) reg.lookup("rmi://127.0.0.1/servicoHora");
+    Hora h = shc.obterHora("Cli1")
+      System.out.println(hr.nomeCliente +" "+ hr.timestamp);
+    
+      
   }
 }
