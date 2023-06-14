4923412c38fcf392944930ec43f30dbb6deaa22e
Samira Haddad <60987282+samyhad@users.noreply.github.com>
2023-06-14 20:53:01 -0300

    Update Cliente.java

--- Napster/src/RMIExample/Cliente.java
+++ Napster/src/RMIExample/Cliente.java
@@ -1,1 +1,10 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.Registry;
 
+public class Cliente {
+
+  public static void main(String[] args){
+    Registry reg = LocateRegistry.getRegistry();
+    reg.lookup("rmi://127.0.0.1/servicohora")
+  }
+}
